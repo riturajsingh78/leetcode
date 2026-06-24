d98b55b6658421a643c4ce38b183a68cd8478bf8
RITURAJ_SINGH <rajput4583@gmail.com>
2026-06-24 12:56:33 +0530

    Time: 4 ms (15.19%), Space: 23.1 MB (30.25%) - LeetHub

--- 0135-candy/0135-candy.cpp
+++ 0135-candy/0135-candy.cpp
@@ -6,23 +6,23 @@ public:
         if(n==0) return 0;
         if(n==1) return 1;
 
-        vector<int>L2R (n,1);
-        vector<int>R2L (n,1);
+        vector<int>count (n,1);
+       
          
 
          for(int i=1;i<n;i++){
             if(ratings[i]>ratings[i-1]){
-                L2R[i]=L2R[i]+L2R[i-1];
+                count[i]=max(count[i],count[i-1]+1);
             }
          }
          for(int i=n-2;i>=0;i--){
             if(ratings[i]>ratings[i+1]){
-                R2L[i]=R2L[i]+R2L[i+1];
+                count[i]=max(count[i],count[i+1]+1);
             }
          }
           int sum =0;
          for(int i=0;i<n;i++){
-            sum = sum+max(L2R[i],R2L[i]);
+            sum +=count[i];
          }
          return sum;
     }
